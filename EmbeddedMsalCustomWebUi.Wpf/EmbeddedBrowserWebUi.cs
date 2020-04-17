3d8225fb8aad7b65810c4ab606f9c20b908d692e
Kazuki Ota <kaota@microsoft.com>
2020-04-17 10:44:25 +0900

    add webui

---
+++ EmbeddedMsalCustomWebUi.Wpf/EmbeddedBrowserWebUi.cs
@@ -0,0 +1,59 @@
+﻿using EmbeddedMsalCustomWebUi.Wpf.Internal;
+using Microsoft.Identity.Client.Extensibility;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EmbeddedMsalCustomWebUi.Wpf
+{
+    /// <summary>
+    /// Provides embedded web ui for WPF on .NET Core.
+    /// The web ui is using WebBrowser control(Trident engine).
+    /// </summary>
+    public class EmbeddedBrowserWebUi : ICustomWebUi
+    {
+        public const int DefaultWindowWidth = 400;
+        public const int DefaultWindowHeight = 800;
+
+        private readonly Window _owner;
+        private readonly string _title;
+        private readonly int _windowWidth;
+        private readonly int _windowHeight;
+        private readonly WindowStartupLocation _windowStartupLocation;
+
+        public EmbeddedBrowserWebUi(Window owner, 
+            string title = "Sign in",
+            int windowWidth = DefaultWindowWidth,
+            int windowHeight = DefaultWindowHeight,
+            WindowStartupLocation windowStartupLocation = WindowStartupLocation.CenterOwner)
+        {
+            _owner = owner ?? throw new ArgumentNullException(nameof(owner));
+            _title = title;
+            _windowWidth = windowWidth;
+            _windowHeight = windowHeight;
+            _windowStartupLocation = windowStartupLocation;
+        }
+
+        public Task<Uri> AcquireAuthorizationCodeAsync(Uri authorizationUri, Uri redirectUri, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<Uri>();
+            _owner.Dispatcher.Invoke(() =>
+            {
+                new EmbeddedWebUiWindow(authorizationUri,
+                    redirectUri,
+                    tcs,
+                    cancellationToken)
+                {
+                    Owner = _owner,
+                    Title = _title,
+                    Width = _windowWidth,
+                    Height = _windowHeight,
+                    WindowStartupLocation = _windowStartupLocation,
+                }.ShowDialog();
+            });
+
+            return tcs.Task;
+        }
+    }
+}
---
+++ EmbeddedMsalCustomWebUi.Wpf/Internal/EmbeddedWebUiWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Identity.Client.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmbeddedMsalCustomWebUi.Wpf.Internal
+{
+    public partial class EmbeddedWebUiWindow : Window
+    {
+        private readonly Uri _authorizationUri;
+        private readonly Uri _redirectUri;
+        private readonly TaskCompletionSource<Uri> _taskCompletionSource;
+        private readonly CancellationToken _cancellationToken;
+        private CancellationTokenRegistration _token;
+
+        public EmbeddedWebUiWindow(
+            Uri authorizationUri,
+            Uri redirectUri,
+            TaskCompletionSource<Uri> taskCompletionSource,
+            CancellationToken cancellationToken)
+        {
+            InitializeComponent();
+            _authorizationUri = authorizationUri;
+            _redirectUri = redirectUri;
+            _taskCompletionSource = taskCompletionSource;
+            _cancellationToken = cancellationToken;
+        }
+
+        private void WebBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (!e.Uri.ToString().StartsWith(_redirectUri.ToString()))
+            {
+                // not redirect uri case
+                return;
+            }
+
+            var query = HttpUtility.ParseQueryString(e.Uri.Query);
+            if (query.AllKeys.Any(x => x == "code"))
+            {
+                _taskCompletionSource.SetResult(e.Uri);
+            }
+            else
+            {
+                _taskCompletionSource.SetException(
+                    new MsalExtensionException(
+                        $"An error occurred, error: {query.Get("error")}, error_description: {query.Get("error_description")}"));
+            }
+
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) => 
+            _token = _cancellationToken.Register(() => _taskCompletionSource.SetCanceled());
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _taskCompletionSource.TrySetCanceled();
+            _token.Dispose();
+        }
+    }
+}
